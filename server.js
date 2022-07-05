b07d8b1f221f04b336f5c00b3e39609d2d3d6d2e
2nd <2nd@dedries-MacBook-Air.local>
2022-07-05 03:09:26 -0400

    added shortid package

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
-
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// app.post('/api/notes', (req, res) =>{
+
+// }
+
+app.get('/api/notes', (req,res)=> {
+    
+}
 
 
 app.listen(PORT, () => {
