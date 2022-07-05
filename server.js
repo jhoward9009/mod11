38310ceb0a748f65d93180a07ab4a6d4e4048006
2nd <2nd@dedries-MacBook-Air.local>
2022-07-05 03:45:43 -0400

    app works

--- server.js
+++ server.js
@@ -27,18 +27,32 @@ app.post('/api/notes', (req,res)=> {
     res.json(getAll)
     
     })
+})
     
-    }
+    })
+    
+
+    app.get('/api/notes', (req, res) => {
+        fs.readFile("./db/db.json", "utf8", (error, results) => {
+    
+    
+            const getAll = JSON.parse(results);
+            res.json(getAll);
+        });
+    
+    });
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/notes.html'));
+    });
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+    
 
 
-// app.get('/api/notes', (req, res) =>{
 
-
-// }
-
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`API server now on port 3001!  http://localhost:${PORT}`);
-// })
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001!  http://localhost:${PORT}`);
+})
