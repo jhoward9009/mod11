bd61dc1f248b3b00a39ccd7232adca7fddaeb7a6
2nd <2nd@dedries-MacBook-Air.local>
2022-07-05 03:01:22 -0400

    ran npm i

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001!  http://localhost:${PORT}`);
+});
